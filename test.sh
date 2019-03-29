ae6f50cd1a5bcaeb1ec593ebd1f6b9520e064f85
MNadir4681 <37255489+MNadir786@users.noreply.github.com>
2019-03-29 12:05:03 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,4 +7,4 @@ echo " this is for agent1 "
 echo "Why it didn't work last time?" 
 echo " Server ip wasn't static changed ip when rebooted added new ip of the new server trying web-hook again."
 echo " Didn't work let me guess " 
-
+echo " It worked and now here to change my ip again and chenged the webhook url as well" 
