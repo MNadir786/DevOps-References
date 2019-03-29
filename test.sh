1c4fd254a4bbdbf9b7488a448832d4ffea69b1f6
MNadir4681 <37255489+MNadir786@users.noreply.github.com>
2019-03-28 18:28:30 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,4 +4,6 @@ echo "this is second line"
 echo "this is third line"
 echo " this is forth line"
 echo " this is for agent1 " 
-Why it didn't work last time? 
+echo "Why it didn't work last time?" 
+echo " Server ip wasn't static changed ip when rebooted added new ip of the new server trying web-hook again."
+
