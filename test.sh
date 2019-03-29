fe40a12f30eb694b283f490ceecfe7d6f28e17b0
MNadir4681 <37255489+MNadir786@users.noreply.github.com>
2019-03-29 12:15:08 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,3 +8,4 @@ echo "Why it didn't work last time?"
 echo " Server ip wasn't static changed ip when rebooted added new ip of the new server trying web-hook again."
 echo " Didn't work let me guess " 
 echo " It worked and now here to change my ip again and chenged the webhook url as well" 
+echo " every time the ip change not only we should update the ip in web-hook url but also in SCM configuration. 
